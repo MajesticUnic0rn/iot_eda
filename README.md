8554a3f6e5c0b1d02f0254cb9d7b2dca0bb3599f
Chris Dong <chrisdong@MacBook-Pro.local>
2023-03-08 12:15:32 -0500

    first commit of pandas feature eng

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # iot_eda
 IOT/ Time series analysis functions
+
+This repo is used for time series analysis for post/pre processing for IOT devices
---
+++ ts_eda.py
@@ -0,0 +1,104 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings("ignore")
+import statistics as stats
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import adfuller
+import statsmodels.api as sm
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf 
+from statsmodels.tsa.arima_model import ARIMA #might not be used for this project
+import ruptures as rpt
+
+def copy_df(df:pd.DataFrame)-> pd.DataFrame:
+   return df.copy(deep=False)
+
+def time_series_ewm(input_df:pd.DataFrame,column:str)-> pd.DataFrame: 
+    data_copy = copy_df(input_df)
+    data_copy[column+"_ewm"] = data_copy[column].ewm(span=12, adjust=False).mean() # take a review of span and adjust 
+    return data_copy
+
+def time_series_rolling(input_df:pd.DataFrame, column:str)-> pd.DataFrame:
+    data_copy = copy_df(input_df)
+    data_copy[column+"_rolling"] = data_copy[column].rolling(window=12).mean()
+    return data_copy
+
+def time_series_log(input_df:pd.DataFrame, column:str)-> pd.DataFrame:
+    data_copy = copy_df(input_df)
+    data_copy[column+"_log"] = np.log(data_copy[column])
+    return data_copy
+
+def time_series_boxcox(input_df:pd.DataFrame, column:str)-> pd.DataFrame:
+    data_copy = copy_df(input_df)
+    data_copy[column+"_boxcox"] = stats.boxcox(data_copy[column])[0]
+    return data_copy
+
+def time_series_seasonal_decompose(input_df:pd.DataFrame, column:str)-> pd.DataFrame:
+    data_copy = copy_df(input_df)
+    data_copy[column+"_seasonal_decomp_trend"] = seasonal_decompose(data_copy[column], model='additive', period=12).trend
+    return data_copy
+
+def time_series_decompose(input_df:pd.DataFrame, column:str)-> pd.DataFrame:
+    data_copy = copy_df(input_df)
+    data_copy[column+"_seasonal_decomp"] = seasonal_decompose(data_copy[column], model='additive', period=12).resid
+    return data_copy
+
+def time_series_difference(input_df:pd.DataFrame, column:str)-> pd.DataFrame:
+    data_copy = copy_df(input_df)
+    data_copy[column+"_diff"] = data_copy[column].diff()
+    return data_copy
+
+def time_series_log_difference(input_df:pd.DataFrame, column:str)-> pd.DataFrame:
+    data_copy = copy_df(input_df)
+    data_copy[column+"_log_difference"] = np.log(data_copy[column]).diff()
+    return data_copy
+
+def time_series_change_point(input_df:pd.DataFrame, column:str)-> pd.DataFrame:
+    data_copy = copy_df(input_df)
+    data_copy[column+"_pct_change"] = data_copy[column].pct_change(periods=12)
+    return 
+    
+def time_series_change_point_model(input_df:pd.DataFrame, column:str)-> list:
+    data_copy = copy_df(input_df)
+    input_values=data_copy[column].values
+    algo = rpt.Pelt(model="rbf").fit(input_values)
+    result = algo.predict(pen=10)
+    return result
+
+def time_series_plot(input_df:pd.DataFrame, column:str)-> None:
+    data_copy = copy_df(input_df)
+    data_copy[column].plot()
+    plt.show()
+    return None
+
+def time_series_interpolate(input_df:pd.DataFrame, column:str)-> pd.DataFrame:
+    data_copy = copy_df(input_df)
+    data_copy[column+"_linear_interpolate"] = data_copy[column].interpolate(method='linear')
+    return data_copy
+
+def time_series_stationarity_test(input_df:pd.DataFrame, column:str)-> None:
+    data_copy = copy_df(input_df)
+    result = adfuller(data_copy[column])
+    print('ADF Statistic: %f' % result[0])
+    print('p-value: %f' % result[1])
+    print('Critical Values:')
+    if result[1] > 0.05:
+        result = "nonstationary"
+    else:
+        result = "stationary"
+    return result
+
+def time_series_plot_acf_pacf(input_df:pd.DataFrame, column:str)-> None:
+    data_copy = copy_df(input_df)
+    fig, ax = plt.subplots(2,1, figsize=(10,6))
+    ax[0] = plot_acf(data_copy[column], lags=50, ax=ax[0])
+    ax[1] = plot_pacf(data_copy[column], lags=50, ax=ax[1])
+    plt.show()
+    return None
+
+def time_series_is_weekend(input_df:pd.DataFrame, column:str)-> pd.DataFrame:
+    data_copy = copy_df(input_df)
+    data_copy[column+"_is_weekend"] = data_copy[column].apply(lambda x: 1 if x.weekday() in [5,6] else 0)
+    return data_copy
